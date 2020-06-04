707127fccf450faea1bffdcffaadfe1b94214339
rahulraj8879 <52400211+rahulraj8879@users.noreply.github.com>
2020-06-04 14:22:41 +0530

    Update Server.java

--- src/com/rahulraj/SocketProgramming/Server.java
+++ src/com/rahulraj/SocketProgramming/Server.java
@@ -9,6 +9,8 @@ public class Server {
     public static void main(String[] args) {
 	// write your code here
       try {
+	      
+	      //open cmd and check if telnet is installed by typing telnet .if not activate it from controlpanel/program-and-feature/turn-window-feature-on-and-off/select telnet and activate
         ServerSocket serverSocket = new ServerSocket(9091);
 
         boolean stop = false;
