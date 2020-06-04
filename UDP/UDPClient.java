d1ff58ded3d5e0790c1717f70b9b028f3634e1d2
rahulraj8879 <52400211+rahulraj8879@users.noreply.github.com>
2020-06-04 16:25:29 +0530

    Add files via upload

---
+++ UDP/UDPClient.java
@@ -0,0 +1,36 @@
+package com.rahulraj.UDP;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPClient {
+  public static void main(String[] args) {
+    try
+    {
+      DatagramSocket clientSocket = new DatagramSocket(0);
+      //DatagramSocket clientSocket = new DatagramSocket();
+      byte[] sendData = new byte[1024];   //store outgoing data.
+      byte[] receiveData = new byte[1024];  //store incoming data
+      //Amount of data = 65535 - 20 (IP Header) - 8 (UDP Header) = 65508
+
+      InetAddress serverAddress = InetAddress.getByName("localhost");
+
+      String stringSendData = "Hello Server!";
+      sendData = stringSendData.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 9090);
+      clientSocket.send(sendPacket);
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      clientSocket.receive(receivePacket);
+      receiveData = receivePacket.getData();
+      String stringReceiveData = new String(receiveData);
+      System.out.println("FROM SERVER: " + stringReceiveData);
+      clientSocket.close();
+    }
+    catch(Exception e)
+    {
+      System.out.println(e.toString());
+    }
+
+  }
+}
